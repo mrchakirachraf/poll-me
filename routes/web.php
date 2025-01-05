cfa9c97f14a2e7d9e387afd145f0c240ff6bae94
mrchakirachraf <achraf.chakir.26.01@gmail.com>
2025-01-05 16:54:40 +0100

    Added angular front end files

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\File;
+
+Route::get('/{any}', function () {
+    $path = public_path('frontend/src/index.html');
+
+    if (File::exists($path)) {
+        return File::get($path);
+    }
+
+    abort(404);
+})->where('any', '.*');
